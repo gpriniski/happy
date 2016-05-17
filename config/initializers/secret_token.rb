d2c311fd9f66728f9cce6aaeed5e3d9601b3fe9d
George Priniski <priniskigeorge@gmail.com>
2016-05-17 18:19:39 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Happy::Application.config.secret_key_base = '4552df703cf49088f2c858d9ca3d2bb655405a5582ec288e10523f1a84db6121dd08c47f2b57d09b6a5f01f72657924ab18ef3fd0443ff709b2089e4b2bddb6c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Happy::Application.config.session_store :cookie_store, key: '_happy_session'
